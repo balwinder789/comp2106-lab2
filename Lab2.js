a8d5560a02fa52bef7828e1927d25e28e5fb8710
balwinder789 <200326404@student.georgianc.on.ca>
2016-09-14 17:42:03 -0400

    update lab2

--- Lab2.js
+++ Lab2.js
@@ -1,8 +1,8 @@
 var userChoice = prompt("Do you choose rock, paper or scissors?");
 var computerChoice = Math.random();
-if (computerChoice < 0.34) {
+if (computerChoice < 0.33 && computerChoice > 0) {
 	computerChoice = "rock";
-} else if(computerChoice <= 0.67) {
+} else if(computerChoice < 0.66 && computerChoice > 0.34) {
 	computerChoice = "paper";
 } else {
 	computerChoice = "scissors";
